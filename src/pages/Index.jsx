5cea2d322acd27ddf81edd297a222bd5b2816752
omarasael1980 <omarasael@nube.unadmexico.mx>
2023-06-15 14:21:20 -0700

    terminando Layout

---
+++ src/pages/Index.jsx
@@ -0,0 +1,11 @@
+
+
+const Index = () => {
+  return (
+    <div>
+      <h1 className="text-6xl text-blue-600 text-center">Desde Index</h1>
+    </div>
+  )
+}
+
+export default Index
---
+++ src/pages/NuevoCliente.jsx
@@ -0,0 +1,11 @@
+
+
+const NuevoCliente = () => {
+  return (
+    <div>
+      <h1 className="text-6xl text-center"> Nuevo Cliente </h1>
+    </div>
+  )
+}
+
+export default NuevoCliente
